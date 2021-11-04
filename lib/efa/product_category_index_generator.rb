557587ba39e961b5916b94d271f5c0738a7cb916
Autumn Wright <autumn@autumncalhoun.com>
2021-11-04 00:09:48 -0400

    Update product category index to use new styles and update fixture

--- lib/efa/product_category_index_generator.rb
+++ lib/efa/product_category_index_generator.rb
@@ -8,19 +8,15 @@ require 'fileutils'
 
 module EFA
   class ProductCategoryIndexGenerator
-    attr_accessor :output_dir
+    attr_accessor :output_dir, :output, :category_rows, :line_break, :tags, :csv_headers
     def initialize(csv_location:, output_dir:)
       @output_dir = output_dir
-      @data = CSV.read(csv_location, headers: true)
-      @styles = { cat: '<ParaStyle:PL-Head1-4>', prod: "<ParaStyle:PL\_Body\_Index-4>" }
+      @category_rows = CSV.read(csv_location, headers: true)
+      template = YAML.safe_load(File.open('./lib/efa/efa.yml')).deep_symbolize_keys
+      @tags = set_tags_from_yml(template)
+      @csv_headers = set_headers_from_yml(template)
       @output = ''
       @line_break = "\n"
-      @header =
-        "<ASCII-MAC>
-<Version:12><FeatureSet:InDesign-Roman><ColorTable:=<Paper:COLOR:CMYK:Process:0,0,0,0><EFA\_Light Blue:COLOR:CMYK:Process:0.6071999669075012,0.23349998891353607,0.21480000019073486,0><Black:COLOR:CMYK:Process:0,0,0,1>>
-<DefineKinsokuStyle:Word\_Kinsoku=>
-<DefineParaStyle:PL-Head1-4=<Nextstyle:PL-Head1-4><cColor:Paper><cTypeface:77 Bold Condensed><cSize:11.000000><cTracking:100><cCase:All Caps><pLeftIndent:2.000000><cLeading:13.000000><pSpaceBefore:13.500000><pSpaceAfter:4.500000><cFont:Helvetica Neue LT Std><pDesiredWordSpace:0.850000><pMaxWordSpace:1.000000><pDesiredLetterspace:-0.050000><pMinLetterspace:-0.050000><pRuleAboveColor:EFA\_Light Blue><pRuleAboveStroke:15.000000><pRuleAboveTint:100.000000><pRuleAboveOffset:-2.880000><pRuleAboveOn:1><pRuleAboveGapColor:None><pRuleBelowGapColor:None><pDropCapDetail:LeftGlyphEdge><cUnderlineGapColor:None><cStrikeThroughGapColor:None><pShadingColor:Pro Black><pWarichuAlignment:Left><bnColor:None><numFont:\<TextFont\>><rUseOTProGlyph:1><cRubyEdgeSpace:1>>
-<DefineParaStyle:PL\_Body\_Index-4=<Nextstyle:PL\_Body\_Index-4><cTypeface:57 Condensed><cSize:11.000000><cAutoPairKern:Optical><cTracking:20><cLeading:13.000000><pHyphenation:0><pTabRuler:154\,Right\,.\,0\,.\;><cFont:Helvetica Neue LT Std><pDesiredWordSpace:0.950000><pMaxWordSpace:1.000000><pMinWordSpace:0.500000><pDesiredLetterspace:-0.100000><pMinLetterspace:-0.150000><pRuleAboveGapColor:None><pRuleBelowGapColor:None><pDropCapDetail:LeftGlyphEdge><cUnderlineGapColor:None><cStrikeThroughGapColor:None><pShadingColor:Pro Black><pWarichuAlignment:Left><bnColor:None><numFont:\<TextFont\>><rUseOTProGlyph:1><cRubyEdgeSpace:1>>\n"
     end
 
     def generate_text
@@ -33,8 +29,8 @@ module EFA
     def write_output_to_file
       FileUtils.mkdir_p output_dir unless Dir.exist? output_dir
       file = File.open(File.join(output_dir, 'ProdCatIndexTT.txt'), 'w')
-      file << @header
-      file << @output
+      file << tags[:header]
+      file << output
       file.close
     end
 
@@ -42,28 +38,38 @@ module EFA
     # be sure to list all subcategories, even if they match the category's name.
     def cat_loop
       # sort by cat, then subcat, then sub subcat
-      cat_index = @data
-
       current_cat = ''
       current_subcat = ''
 
-      cat_index.each do |c|
+      category_rows.each do |c|
         # Loop through and write the cat, subcat or subsubcat if it has not already been written (so it is unique)
         cat = c['CategoryPath'].split('->')[0]
         subcat = c['CategoryName'] || ''
 
         if current_cat != cat
-          @output << @styles[:cat] + cat + @line_break
+          output << tags[:category] + cat + line_break
           current_cat = cat
         end
 
         if current_subcat != subcat
-          @output << @styles[:prod] + subcat + "\t" + @line_break
+          output << tags[:product] + subcat + "\t" + line_break
           current_subcat = subcat
         end
       end
 
-      @output
+      output
+    end
+
+    def set_headers_from_yml(template)
+      template[:csv_files][:Company_Category][:csv_headers]
+    end
+
+    def set_tags_from_yml(template)
+      find_defs(template)[:tags]
+    end
+
+    def find_defs(template)
+      template[:tagged_text_files][:ProdCatIndexTT]
     end
   end
 end
